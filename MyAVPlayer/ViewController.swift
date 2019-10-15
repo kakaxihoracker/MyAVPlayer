93a3e6c75caec5d712c66d08b90ad13b74335f9e
kakaxi <1126298633@qq.com>
2019-10-16 00:31:22 +0800

    test

--- MyAVPlayer/ViewController.swift
+++ MyAVPlayer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
