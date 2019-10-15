02fc46765d416b94d4976745a0fe95bcc357359f
kakaxi <1126298633@qq.com>
2019-10-16 00:02:21 +0800

    初次创建本地仓 并上传

---
+++ MyAVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAVPlayer
+//
+//  Created by 徐潇 on 2019/10/15.
+//  Copyright © 2019 徐潇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
